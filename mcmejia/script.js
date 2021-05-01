09491134e58f6704bb4bdf9bf9060d6afdd94d18
mariacarolinamejia <81448955+mariacarolinamejia@users.noreply.github.com>
2021-04-30 19:19:40 -0500

    Delete mcmejia directory
    eliminar

--- mcmejia/script.js
+++
@@ -1,6 +0,0 @@
-var menuToggle = document.querySelector(".menu-toggle");
-var menu = document.getElementById("menu");
-
-menuToggle.addEventListener ("click", (e)=>{
-    menu.classlist.toggle("mostrar")
-});
